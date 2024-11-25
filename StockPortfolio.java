a0faa713c1016d22faa42428d3ac4d60dd465ea9
JayasuryaThirumaran <jayasurya.t@talentsprint.com>
2024-11-25 18:51:16 +0000

    Changes to be committed: 	modified:   .devcontainer/devcontainer.json 	modified:   StockPortfolio.java 	modified:   main.java

--- StockPortfolio.java
+++ StockPortfolio.java
@@ -1,29 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        // Test Case 1: Stock Analysis
+        StockPortfolio stock = new StockPortfolio("AAPL", 10, 150.0);
+        stock.updatePrice(160.0);
+        System.out.println(stock.calculateProfit());
+        System.out.println(stock.getCurrentValue());
+    }
+}
+
 class StockPortfolio {
-    // TODO: Add necessary fields for stock details
     private String symbol;
     private int quantity;
     private double buyingPrice;
     private double currentPrice;
+    private double highestPrice;
 
-    // TODO: Initialize stock details
     public StockPortfolio(String symbol, int quantity, double buyingPrice) {
-        // TODO
+        this.symbol = symbol;
+        this.quantity = quantity;
+        this.buyingPrice = buyingPrice;
+        this.currentPrice = buyingPrice;
+        this.highestPrice = buyingPrice;
     }
 
-    // TODO: Update current market price
     public void updatePrice(double newPrice) {
-        // TODO
+        if (newPrice > 0) {
+            this.currentPrice = newPrice;
+            if (newPrice > this.highestPrice) {
+                this.highestPrice = newPrice;
+            }
+        }
     }
 
-    // TODO: Calculate total profit or loss
     public double calculateProfit() {
-        // TODO
-        return 0.0;
+        return (currentPrice - buyingPrice) * quantity;
     }
 
-    // TODO: Calculate current total value of holdings
     public double getCurrentValue() {
-        // TODO
-        return 0.0;
+        return currentPrice * quantity;
     }
 }
